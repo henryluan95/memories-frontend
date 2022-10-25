726f5da1afdbaececea38e6db99aab16833fd49d
Henry Luan <henryluan95@gmail.com>
2022-10-25 19:01:32 -0400

    add functionality to the form

--- src/api/index.js
+++ src/api/index.js
@@ -6,3 +6,7 @@ const url = `http://localhost:5000/posts`;
 export const fetchPosts = () => {
   return axios.get(url);
 };
+
+export const createPost = (newPost) => {
+  return axios.post(url, newPost);
+};
